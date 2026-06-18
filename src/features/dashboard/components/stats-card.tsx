41861a534c5357e06c8029c23df250af61c55dc4
Aurelie Raout <aurelieraout@gmail.com>
2026-06-18 19:05:27 +0200

    feat: add client management components and update dashboard stats

--- src/features/dashboard/components/stats-card.tsx
+++ src/features/dashboard/components/stats-card.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link'
+
 function StatsCard() {
   return (
-    <main className="flex flex-col col-auto items-center justify-center border-2 border-gray-300 rounded-lg p-8 m-8">
-      <h3 className="text-4xl font-bold text-center pb-6">Stats</h3>
-      <p>Nombre de clients : 10</p>
-      <p>Nombre de sessions à venir : 5</p>
-      <p>Factures à éditer : 2</p>
+    <main className="flex flex-col col-auto items-center justify-center border-2 border-gray-300 rounded-lg p-8 m-8 gap-4">
+      <h3 className="text-4xl font-bold text-center pb-6 ">Stats</h3>
+      <Link href="/clients">
+        <p className="cursor-pointer">Nombre de clients : 10</p>
+      </Link>
+      <Link href="/upcoming-sessions">
+        <p className="cursor-pointer">Nombre de sessions à venir : 5</p>
+      </Link>
+      <Link href="/invoices">
+        <p className="cursor-pointer">Factures à éditer : 2</p>
+      </Link>
     </main>
   )
 }
