f528ac69fe0f6bbd2934b2b0dee3d93bb8063959
Aurelie Raout <aurelieraout@gmail.com>
2026-06-22 18:34:24 +0200

    style: archived badge putted on the far right

--- src/features/clients/components/client-card.tsx
+++ src/features/clients/components/client-card.tsx
@@ -32,13 +32,15 @@ function ClientCard({
           </div>
         </div>
 
-        <div className=" flex items-center justify-around">
+        <div className=" flex items-center justify-between w-full">
           <p className="text-sm bg-main-500 text-white px-1.5 py-0.5 rounded-lg inline-block mb-2">
             {level}
           </p>
-          <p className="italic text-sm bg-main-900/30 text-main-50 px-1.5 py-0.5 rounded-lg inline-block mb-2">
-            {active ? '' : 'Archivé'}
-          </p>
+          {!active && (
+            <p className="italic text-sm bg-main-900/30 text-main-50 px-1.5 py-0.5 rounded-lg inline-block mb-2">
+              Archivé
+            </p>
+          )}
         </div>
         <p>{notes || 'Aucune note'}</p>
       </Card>
